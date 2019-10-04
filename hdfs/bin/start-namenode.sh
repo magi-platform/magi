cc0f0cfe3a996eca97d1f1adf38307b43d897d48
Michael Reynolds <michael.reynolds@twosixlabs.com>
2019-10-04 19:17:56 -0400

    [DEBUG] - need to print out the directory when the namenode is recovered

--- hdfs/bin/start-namenode.sh
+++ hdfs/bin/start-namenode.sh
@@ -3,6 +3,8 @@
 
 IS_FORMATTED=$(find $HADOOP_NAMENODE_DIR -name VERSION)
 
+echo "found existing hdfs directory : $IS_FORMATTED"
+
 if [ -z $IS_FORMATTED ]; then
     echo "Formatting a new name node"
     $HADOOP_HOME/bin/hdfs namenode -format -force
