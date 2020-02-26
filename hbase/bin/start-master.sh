4fd5f4039c28d85d5ecb0b9ca31f121af460718a
Yan Zverev <yanz67@users.noreply.github.com>
2020-02-26 14:52:13 -0500

    [MAGI-31] - Resolves magi-platform/magi#31 and makes pause time configurable (#32)
    the build is broken

--- hbase/bin/start-master.sh
+++ hbase/bin/start-master.sh
@@ -3,6 +3,8 @@
 
 touch /opt/hbase/logs/hbase.log
 
+SCHEMA_IMPORT_PAUSE=${SCHEMA_IMPORT_PAUSE:=60}
+
 echo "Starting HBase Master"
 /opt/hbase/bin/hbase master start >> /opt/hbase/logs/hbase.log &
 
@@ -10,7 +12,7 @@ if [ -z "$SCHEMA_PATH" ]; then
     echo "no schema to import"
 else
     echo "importing schema instructions from $SCHEMA_PATH"
-    sleep 30
+    sleep $SCHEMA_IMPORT_PAUSE
     /opt/hbase/bin/hbase shell -n $SCHEMA_PATH
 fi
 
